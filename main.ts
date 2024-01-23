865edd3b8ff5a38035513c19d061415dff799e58
dycimage <2424748461@qq.com>
2024-01-23 12:40:35 +0800

    support liveView and readView

--- main.ts
+++ main.ts
@@ -1,17 +1,20 @@
-import { Platform, Plugin, TFile, MarkdownView } from "obsidian";
+import {Platform, Plugin, TFile, MarkdownView, editorLivePreviewField} from "obsidian";
 
-export default class HtmlLocalSrcPlugin extends Plugin {
+export  default class HtmlLocalSrcPlugin extends Plugin {
 	private activeFile: TFile | null = null;
+
 	onload() {
+		this.app.workspace.getActiveViewOfType(MarkdownView);
 		this.registerMarkdownPostProcessor((element, ctx) => {
-			this.processMarkdown(element, ctx);
+			this.processMarkdown(element);
 
 		});
 
 		this.registerMarkdownPostProcessor(this.modifyHTML.bind(this));
-		this.registerEvent(this.app.workspace.on("file-open",this.show.bind(this)));
+		this.registerEvent(this.app.workspace.on("file-open", this.processView.bind(this)));
 	}
-	processMarkdown(element: HTMLElement, ctx: { sourcePath: string }){
+
+	processMarkdown(element: HTMLElement) {
 		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
 		const activeFile = activeView?.file;
 		// console.log(element);
@@ -20,7 +23,7 @@ export default class HtmlLocalSrcPlugin extends Plugin {
 
 			const targetLinks = Array.from(element.getElementsByTagName("img")).filter(
 				(link) => {
-					// console.log(link.src);
+					console.log('live', editorLivePreviewField, 'live');
 					return link.src.lastIndexOf(':') === 3;
 				}
 			);
@@ -46,13 +49,17 @@ export default class HtmlLocalSrcPlugin extends Plugin {
 			}
 		}
 	}
-	useProcessMarkdown(file:TFile){
+
+	useProcessMarkdown(file: TFile) {
+
 		// 在 handleFileOpen 中调用 processMarkdown
 		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		console.log('activeView  ',activeView);
 		const element = activeView?.contentEl as HTMLElement;
-		const ctx = { sourcePath: file.path };
-		this.processMarkdown(element, ctx);
+		const ctx = {sourcePath: file.path};
+		this.processMarkdown(element);
 	}
+
 	handleFileOpen = async (file: TFile) => {
 		// 当文件打开时，移除之前的 modify 事件监听器
 		console.log('in handleFileOpen');
@@ -65,7 +72,7 @@ export default class HtmlLocalSrcPlugin extends Plugin {
 		this.activeFile = file;
 
 		// 添加当前活动文件的 modify 事件监听器
-		this.app.vault.on("modify", this.handleFileModify);
+		this.registerEvent(this.app.vault.on("modify", this.handleFileModify));
 	};
 
 	handleFileModify = async (file: TFile) => {
@@ -75,18 +82,92 @@ export default class HtmlLocalSrcPlugin extends Plugin {
 			this.useProcessMarkdown(file);
 		}
 	};
+
 	modifyHTML(el: HTMLElement, ctx: { sourcePath: string }) {
 		// Perform additional modifications to HTML if needed
 		console.log("Markdown rendering completed:", ctx.sourcePath);
 	}
-	show(file:TFile){
-		console.log('文件已打开',file.path);
+
+	/*
+	scroll部分子函数
+	 */
+
+	scrollLines = async (element: HTMLElement): Promise<number> => {
+
+		// 获取一个元素，你可以根据你的需要选择合适的选择器
+// 使用 as 类型断言，告诉编译器 element 是一个 HTMLElement 类型的对象
+// 		let element = document.querySelector('.cm-scroller') as HTMLElement;
+
+// 获取元素的边界矩形
+		let rect = element.getBoundingClientRect();
+
+// 获取元素的高度，可见高度，内容高度，滚动距离 总长度
+// 使用 const 声明常量，并添加类型注解
+		const height: number = rect.height;
+		const clientHeight: number = Math.round(element.clientHeight);
+		// const scrollHeight: number = element.scrollHeight;
+		const scrollTop: number = Math.round(element.scrollTop);
+		const scrollHeight:number = element.scrollHeight
+		// element.
+
+// 计算元素的每一行的高度，假设每一行的高度是相同的
+// 使用 const 声明常量，并添加类型注解
+		const lineHeight: number = height / clientHeight;
+
+// 计算元素的总行数，假设每一行的内容是相同的
+// 使用 const 声明常量，并添加类型注解
+// 		const totalLines: number = scrollHeight / lineHeight ;
+
+// 计算元素的滚动行数，也就是滚动了多少行
+// 使用 const 声明常量，并添加类型注解
+		const scrollLines: number = Math.round(scrollTop / lineHeight);
+		console.log('scrollHeight ',scrollTop%clientHeight -clientHeight/4 <0)
+		if (scrollTop%clientHeight -clientHeight/4 <0){
+			console.log('in Scroll');
+			this.useProcessMarkdown(this.activeFile as TFile)
+		}
+
+
+// 在控制台打印出滚动行数
+		console.log('You scrollTop ' + scrollTop+'  ,Height ='+clientHeight + ', clientHeight ' + clientHeight + ' lines.');
+		return scrollLines;
+	};
+
+
+	scroll_toUpdateView(activeView:MarkdownView) {
+		let div = activeView?.containerEl.querySelector('.cm-scroller');
+		console.log(div)
+		// @ts-ignore
+		div.getBoundingClientRect();
+// this.registerDomEvent()
+
+		// @ts-ignore
+		this.registerDomEvent(div, 'scroll', (event) => {
+			this.scrollLines(div as HTMLElement);
+			// 在控制台打印出滚动的距离
+			console.log('Editor Scrolled:   pixels vertically and pixels horizontally.');
+		});
+	}
+
+	/*
+	主要函数
+	 */
+	processView() {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		const file = activeView?.file;
+		console.log(activeView?.file);
+		if (file==null) return;
+		console.log('文件已打开', file.path);
 		// const vault = JSON.parse(file.vault.);
-		let attr = (file.vault as any).config.livePreview
-		if(attr){
+		const config = (file.vault as any).config;
+		let attr = config.livePreview;
+		const defaultView = config.defaultViewMode;
+		console.log(file);
+		if (attr || defaultView === 'preview') {
 			// this.processMarkdown();
 			console.log(attr);
 			this.handleFileOpen(file);
+			this.scroll_toUpdateView(activeView as MarkdownView);
 
 		}
 		console.log('end');
